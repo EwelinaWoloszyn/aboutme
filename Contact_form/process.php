593ea1c1d55be66b8d71c5aaa943ac5c09311dbe
EwelinaWoloszyn <woloszynewelina1@gmail.com>
2024-02-06 17:16:43 +0100

    Update process.php

--- Contact_form/process.php
+++ Contact_form/process.php
@@ -8,7 +8,7 @@ $emailTo = 'woloszynewelina1@gmail.com'; // or pull from PW page field
 $form = array(
     'name' => $sanitizer->text($input->post->fullname),
     'email' => $sanitizer->email($input->post->email),
-    'phone' => $sanitizer->textarea($input->post->comments),
+    'phone' => $sanitizer->textarea($input->post->phone),
     ); 
 
 // check if the form was submitted
@@ -23,7 +23,7 @@ if($input->post->submit) {
     if(!$error) {
         $msg = "Full name: $form[fullname]\n" . 
                "Email: $form[email]\n" . 
-               "Comments: $form[comments]"; 
+               "Phone: $form[phone]"; 
 
         mail($emailTo, "Website contact form submission", "$form[comments]", "From: $form[email]");
 
@@ -40,52 +40,9 @@ if(!$sent) {
         $form[$key] = htmlentities($value, ENT_QUOTES, "UTF-8"); 
     }
 
-    // append form to body copy
-    $page->body .= <<< _OUT
-    	$error
-        <form action="./" method="post" id="contact-form">
-			<fieldset>
-				<legend>Send a note</legend>
-					<ol>
-						<li class="form-row">
-							<span class="error" style="display: none;" ></span>
-						</li>
-						<li class="form-row">
-							<input id="fullname" name="fullname" type="text" size="30" class="name required default" title="Your name" value="$form[fullname]"/>
-						</li>
-						<li class="form-row">
-							<input id="inputemail" name="email" type="text" size="30" class="required email default" title="Your email address" value="$form[email]" />
-						</li>
-						<li class="form-row">
-							<textarea name='comments' rows='5' cols='45' id='comments' title="Your message">$form[comments]</textarea>
-						</li>
-						<li class="form-row">
-							<input type="submit" name="submit" value="Send" class="submit-button"/>
-						</li>
-					</ol>
-			</fieldset>
-        </form>
+
 
 _OUT;
 
 }
-?><?php 
-
-/**
- * Contact form template
- *
- */
-
-include("./header.inc"); 
 ?>
-        <div class="main-container">
-            <div class="main wrapper clearfix">
-                <article>
-                    <section>
-	                    <?php echo $page->body; ?>
-                    </section>
-                </article>
-            </div> <!-- #main -->
-        </div> <!-- #main-container -->
-<?
-include("./footer.inc"); 
