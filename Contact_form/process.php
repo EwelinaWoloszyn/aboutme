cb7e260919b297702c2778614efe234170325ed9
EwelinaWoloszyn <woloszynewelina1@gmail.com>
2024-02-06 17:44:12 +0100

    Update process.php no comments

--- Contact_form/process.php
+++ Contact_form/process.php
@@ -11,15 +11,15 @@ $form = array(
     'phone' => $sanitizer->textarea($input->post->phone),
     ); 
 
-// check if the form was submitted
+
 if($input->post->submit) {
         
-    // determine if any fields were ommitted or didn't validate
+    
     foreach($form as $key => $value) {
         if(empty($value)) $error = "<p class='error'>Please check that you have completed all fields.</p>";
     }
 
-    // if no errors, email the form results
+    
     if(!$error) {
         $msg = "Full name: $form[fullname]\n" . 
                "Email: $form[email]\n" . 
@@ -27,22 +27,11 @@ if($input->post->submit) {
 
         mail($emailTo, "Website contact form submission", "$form[comments]", "From: $form[email]");
 
-        // populate body with success message, or pull it from another PW field
+
         $page->body = "<div id='message-success'><p>Thanks, your message has been sent.</p></div>"; 
         $sent = true;   
     }
 }
 
-if(!$sent) {
 
-    // sanitize values for placement in markup
-    foreach($form as $key => $value) {
-        $form[$key] = htmlentities($value, ENT_QUOTES, "UTF-8"); 
-    }
-
-
-
-_OUT;
-
-}
 ?>
